d98f718cf6044166c24d7ad26037f087cbdea4e5
sreevidhyaa08 <23b01a05g4@svecw.edu.in>
2025-02-17 11:36:31 +0530

    Create fintech.py

---
+++ fintech.py
@@ -0,0 +1,8 @@
+class BudgetTracker:
+    def track(self, data):
+        # Dummy implementation
+        return {
+            'total_budget': 1000,
+            'spent': 450,
+            'remaining': 550
+        }
