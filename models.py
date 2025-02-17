a1595ad481cfa8d82f6b56505eb27ad40f37ced6
sreevidhyaa08 <23b01a05g4@svecw.edu.in>
2025-02-17 11:35:10 +0530

    models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+import random
+
+class SafetyScoreModel:
+    def predict(self, location):
+        # Dummy implementation
+        return random.uniform(1, 10)
+
+class ItineraryPlanner:
+    def plan(self, data):
+        # Dummy implementation
+        return {
+            'day1': 'Visit local museum',
+            'day2': 'Hiking in national park',
+            'day3': 'Relax at the beach'
+        }
