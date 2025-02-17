a3dbba481a1f9253f93f214b8580d3ff1811ab11
sreevidhyaa08 <23b01a05g4@svecw.edu.in>
2025-02-17 11:36:01 +0530

    community.py

---
+++ community.py
@@ -0,0 +1,7 @@
+class CommunitySupport:
+    def get_alerts(self, data):
+        # Dummy implementation
+        return [
+            'Alert: Pickpocket reported near central station',
+            'Alert: Protest in downtown area, avoid if possible'
+        ]
